ff7ee586e1f3e6f889dc0c2bbea971b17f3e44d8
Arthur <n.artur2281337@gmail.com>
2021-12-24 17:07:52 +0200

    add HashRouter & npm scripts

--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -1,17 +1,17 @@
 import { Navbar } from "components/Navbar";
 import { Favourites } from "pages/Favourites";
 import { RandomDish } from "pages/RandomDish";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 export function Routing() {
   return (
-    <Router>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<RandomDish />} />
         <Route path="/random" element={<RandomDish />} />
         <Route path="/favourites" element={<Favourites />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
